4001fefe59fd358d4ae1563fb50137212de7ace7
Favio Manriquez Leon <favio@favrik.com>
2014-04-25 10:28:56 -0700

    Make character items generation simpler

--- character_nav.jsx
+++ character_nav.jsx
@@ -3,39 +3,19 @@
 var CharacterNav = React.createClass({
   getDefaultProps: function () {
     return {
-      showMissing: true,
+      extra: []
     };
   },
 
-  getItems: function () {
-    var initials = [];
-    for (var i = 0, c = this.props.data.length; i < c; i++) {
-      var character = this.props.data[i][this.props.label].charAt(0);
-      if (initials.indexOf(character) == -1) {
-        initials.push(character);
-      }
-    }
-
-    return initials.sort();
-  },
-
-  getItemsWithMissing: function () {
-    var characters = this.getItems();
-    var abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-
-    var diff = abc.filter(function(i) {
-      return characters.indexOf(i) < 0;
-    });
-
-    return characters.concat(diff).sort();
-  },
-
   select: function (character) {
-    
   },
 
   render: function () {
-    var characters = this.props.showMissing ? this.getItemsWithMissing() : this.getItems();
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      .split('')
+      .concat(this.props.extra)
+      .sort();
+
     var items = characters.map(function (item, index) {
       return <CharacterNavItem character={item} />;
     });
